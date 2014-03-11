c1fb6677bf8493e86d3d431f6dddfefa31056db1
Jose Minguez <jpminguez@gmail.com>
2014-03-11 18:15:05 +0100

    change package.json "name" to node-wemo

--- test/example.js
+++ test/example.js
@@ -7,13 +7,11 @@ var testWemoControler = function(uuid){
 	setTimeout( function(){wemoController.switchOff(uuid);} , 3000 );
 	setTimeout( function(){wemoController.switchOn(uuid);} , 5000 );
 	setTimeout( function(){wemoController.switchOff(uuid);} , 7000 );
-
-
+	
 	wemoController.onDeviceStatusChange(uuid, function(deviceId, status){
-		console.log('Status changed for device ' + deviceId + JSON.stringify(status))
+		console.log('Status changed for device ' + deviceId + ': ' + JSON.stringify(status))
 	});
 
 }
-
 
 wemoController.onNewDevice(testWemoControler);
